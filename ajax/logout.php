6e1ad655f966c9bd59803382e66e9cddec3b8555
DadaKobeleva <kobelevadara@yandex.ru>
2025-12-04 13:32:30 +0500

    Выход из системы

--- ajax/logout.php
+++ ajax/logout.php
@@ -1,4 +1,27 @@
 <?php
 	session_start();
+	require_once("../settings/connect_datebase.php");
+
+	$IdUser = $_SESSION["user"];
+	$IdSession = $_SESSION["IdSession"];
+
+	$Sql = "SELECT `session`.*, `users`.`login` ".
+	"FROM `session` `session` ".
+	"JOIN `users` `users` ON `users`.`id` = `session`.`IdUser`". 
+	"WHERE `session`.`Id` = {$IdSession}";
+
+	$Query = $mysqli->query(query: $Sql);
+	$Read = $Query->fetch_array();
+
+	$TimeStart = strtotime(datetime: $Read["DateStart"]);
+	$TimeNow = time();
+	$Ip = $Read["Ip"];
+	$TimeDelta = gmdate(format: "H:i:s", timestamp: ($TimeNow - $TimeStart));
+	$Date = date(format: "Y-m-d H:i:s");
+	$Login = $Read["login"];
+
+	$Sql = "INSERT INTO `logs`(`Ip`, `IdUser`, `Date`, `TimeOnline`, `Event`) VALUES ('{$Ip}','{$IdUser}','{$Date}','{$TimeDelta}','Пользователь {$Login} покинул сайт')";
+	$mysqli->query($Sql);
+
 	session_destroy();
 ?>
