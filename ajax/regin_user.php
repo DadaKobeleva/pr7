abc40b3b861f7767b3970bbd38800ff7c7c1c6e1
DadaKobeleva <kobelevadara@yandex.ru>
2025-12-11 00:48:53 +0500

    Регистрация

--- ajax/regin_user.php
+++ ajax/regin_user.php
@@ -5,11 +5,12 @@
 	$login = $_POST['login'];
 	$password = $_POST['password'];
 	
-	// ищем пользователя
+	// ищем пользователя с таким логином
 	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."'");
 	$id = -1;
 	
 	if($user_read = $query_user->fetch_row()) {
+		// Пользователь уже существует
 		echo $id;
 	} else {
 		$mysqli->query("INSERT INTO `users`(`login`, `password`, `roll`) VALUES ('".$login."', '".$password."', 0)");
@@ -18,7 +19,26 @@
 		$user_new = $query_user->fetch_row();
 		$id = $user_new[0];
 			
-		if($id != -1) $_SESSION['user'] = $id; // запоминаем пользователя
+		if($id != -1) {
+			$_SESSION['user'] = $id; // запоминаем пользователя
+
+			$Ip = $_SERVER["REMOTE_ADDR"];
+			$DateStart = date(format: "Y-m-d H:i:s");
+
+			# Пользователь зарегистрировался
+			# 1. Создать сессию
+			$Sql = "INSERT INTO `session`(`IdUser`, `Ip`, `DateStart`, `DateNow`) VALUES ('{$id}', '{$Ip}', '{$DateStart}' , '{$DateStart}')";
+			$mysqli->query(query: $Sql);
+
+			$Sql = "SELECT `Id` FROM `session` WHERE `DateStart` = '{$DateStart}';";
+			$Query = $mysqli->query(query: $Sql);
+			$Read = $Query->fetch_assoc();
+			$_SESSION["IdSession"] = $Read["Id"];
+
+			# 2. Записать событие регистрации
+			$Sql = "INSERT INTO `logs`(`Ip`, `IdUser`, `Date`, `TimeOnline`, `Event`) VALUES ('{$Ip}','{$id}','{$DateStart}','00:00:00','Пользователь ($login) зарегистрировался и авторизовался.')";
+			$mysqli->query(query: $Sql);
+		}
 		echo $id;
 	}
 ?>
