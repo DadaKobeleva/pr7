d5d7301bf6667d418be9f366ecc8007d667aae6c
DadaKobeleva <kobelevadara@yandex.ru>
2025-12-11 01:11:37 +0500

    Востановление пароля

--- ajax/recovery.php
+++ ajax/recovery.php
@@ -27,9 +27,9 @@
 		return $password;
 	}
 	
-	if($id != 0) {
+	if($id != -1) {
 		//обновляем пароль
-		$password = PasswordGeneration();;
+		$password = PasswordGeneration();
 		// проверяем не используется ли пароль 
 		$query_password = $mysqli->query("SELECT * FROM `users` WHERE `password`= '".md5($password)."';");
 		while($password_read = $query_password->fetch_row()) {
@@ -38,9 +38,24 @@
 		}
 		// обновляем пароль
 		$mysqli->query("UPDATE `users` SET `password`='".md5($password)."' WHERE `login` = '".$login."'");
-		// отсылаем на почту
-		//mail($login, 'Безопасность web-приложений КГАПОУ "Авиатехникум"', "Ваш пароль был только что изменён. Новый пароль: ".$password);
+		
+		$Ip = $_SERVER["REMOTE_ADDR"];
+		$Date = date(format: "Y-m-d H:i:s");
+		
+		if(isset($_SESSION['user'])) {
+			$current_user_id = $_SESSION['user'];
+		} else {
+			$current_user_id = $id;
+		}
+		
+		$Sql = "INSERT INTO `logs`(`Ip`, `IdUser`, `Date`, `TimeOnline`, `Event`) VALUES ('{$Ip}','{$current_user_id}','{$Date}','00:00:00','Пароль пользователя {$login} (ID: {$id}) был сброшен. Новый пароль: {$password}')";
+		$mysqli->query(query: $Sql);
+		
+		// отсылаем на почту (раскомментировать когда будет настроена отправка)
+		// mail($login, 'Безопасность web-приложений КГАПОУ "Авиатехникум"', "Ваш пароль был только что изменён. Новый пароль: ".$password);
+		
+		echo $password;
+	} else {
+		echo "error";
 	}
-	
-	echo $id;
 ?>
